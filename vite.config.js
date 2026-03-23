24b7a5d37a2c8f815fe28f6a8ca5259830d84265
nigelgummerson <nigelgummerson@gmail.com>
2026-03-23 16:55:41 +0000

    build: switch default build to code-split web output

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
+  base: '/spine/',
   build: {
     target: 'es2020',
-    assetsInlineLimit: 100000000,
   },
 });
